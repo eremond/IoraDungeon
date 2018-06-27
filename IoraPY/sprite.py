f54e2976f34feefe1b2de206163412d01e1c4cb0
Andrew Dilanchian <39834786+adilanchian96@users.noreply.github.com>
2018-06-27 17:33:20 -0400

    Edited for readability

--- IoraPY/sprite.py
+++ IoraPY/sprite.py
@@ -1,39 +1,35 @@
 import os
-import math
 import pygame
-from pygame.locals import *
 
-class sprite(pygame.sprite.Sprite):
-    def __init__(self, x=0, y=0, s=20, image=""):
-        super(GameObject, self).__init__()
-        if not image:
-            image = self.image
-        setRect(image, s)
-        self.rect.x = x
-        self.rect.y = y
+class sprites(pygame.sprite.Sprite):
+    def __init__(self, image):     #image should be a string to the path of the image
+        pygame.sprite.Sprite.__init__(self)
         self.isType = "" #"type(obj) is type" can also be used in tandem
-
-    def setRect(self, img, s):
-        self.image = pygame.transform.scale(pygame.image.load(img), (s, s)) #s is size of rect
+        self.image = pygame.image.load(image).convert_alpha()   #convert_alpha creates transparent background
         self.rect = self.image.get_rect()
+
+    def transformImage(self, img, x, y):
+        self.image = pygame.transform.scale(img, (x, y)) #x, y is new size of image
 
     def getTop(self):
         return self.rect.top
+
     def getBottom(self):
         return self.rect.bottom
+
     def getLeft(self):
         return self.rect.left
+        
     def getRight(self):
         return self.rect.right
 
     def loadAnimSprite(self, path):
         images = []
         for file in os.listdir(path):
-            image = pygame.image.load(path + os.sep + file).convert()
-            images.append(image)
+            images.append(pygame.image.load(path + os.sep + file).convert_alpha())
         return images
 
-  """ def canMove(self, xCh, yCh, group):
+    """ def canMove(self, xCh, yCh, group):
         old_pos = self.rect.x, self.rect.y
         self.rect.x += xCh
         self.rect.y += yCh
@@ -55,5 +51,3 @@ class sprite(pygame.sprite.Sprite):
         self.rect.x = old_pos[0]
         self.rect.y = old_pos[1]
         return False, xCh, yCh, collided_with """
-
-
