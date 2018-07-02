718d4f54f9a77fe1c6ab062f4b07baf3f4bd0e37
Andrew Dilanchian <39834786+adilanchian96@users.noreply.github.com>
2018-07-02 00:04:43 -0400

    Added some new features
    added projectiles, enemy collision (with projectiles), and 'switching weapons'
    
    Also cleaned up some stuff and added more comments for readability

--- IoraPY/main.py
+++ IoraPY/main.py
@@ -1,6 +1,7 @@
 import pygame
 import levels
 import sprites
+import projectiles
 print(pygame.__path__)
 pygame.init()
 
@@ -17,28 +18,32 @@ def animGen(animation):     #helps the move function to generate the next image 
             x+=1
             a = 0
 
-
-def move(key, sprite, *animGens, animations):
+def move(key, sprite, *animGens, animations):       #can probably be thrown in an update function
     uAnim, dAnim, lAnim, rAnim = animGens
 
     if key[pygame.K_DOWN]:
         if sprite.rect.y < 480-16-sprite.rect.height:
             sprite.rect.y+=2
-        return next(dAnim)
-    elif key[pygame.K_UP]:
+        sprite.image = next(dAnim)
+        return "down"
+    if key[pygame.K_UP]:
         if sprite.rect.y > 16:
             sprite.rect.y-=2
-        return next(uAnim)
-    elif key[pygame.K_LEFT]:
+        sprite.image = next(uAnim)
+        return "up"
+    if key[pygame.K_LEFT]:
         if sprite.rect.x > 16:
             sprite.rect.x-=2
-        return next(lAnim)
-    elif key[pygame.K_RIGHT]:
+        sprite.image = next(lAnim)
+        return "left"
+    if key[pygame.K_RIGHT]:
         if sprite.rect.x < 640-16-sprite.rect.width:
             sprite.rect.x+=2
-        return next(rAnim)
+        sprite.image = next(rAnim)
+        return "right"
     else:
-        return animations[1][1]
+        sprite.image = animations[1][1]
+        return "down"
 
 
 #--------------------Set some variables-------------------
@@ -47,49 +52,72 @@ height = 480
 size = width, height
 screen = pygame.display.set_mode(size)  #opens the physical screen
 clock = pygame.time.Clock()     #keeps track of the fps and stuff
+
+
 #--------------Create some sprite variables--------------
 player = sprites.sprites('Character/DownAnim/Down2.png')
+enemy = sprites.sprites('Character/DownAnim/Down2.png')
 player.isType = "player"
 leftAnim = player.loadAnimSprite('Character/LeftAnim')
 rightAnim = player.loadAnimSprite('Character/RightAnim')
 upAnim = player.loadAnimSprite('Character/UpAnim')
 downAnim = player.loadAnimSprite('Character/DownAnim')
+element = ['projectiles/fireProj.png', 'projectiles/iceProj.png', 'projectiles/lightProj.png']
 uAnim = animGen(upAnim)
 dAnim = animGen(downAnim)
 lAnim = animGen(leftAnim) 
 rAnim = animGen(rightAnim)
+enemy.center(width, height)
+magic = pygame.sprite.Group()
+enemies = [enemy]
+enemyGroup = pygame.sprite.Group(enemies)
 
 #-----------------------Load images-----------------------
-img = pygame.image.load('images/bot_wall.jpg').convert()
-
-wallDown = pygame.image.load('images/Side_Walls_05.jpg').convert()
-
-sFloor = pygame.image.load('images/Left_edge_floor.jpg').convert()
-
-floor = pygame.image.load('images/floor.jpg').convert()
-
-tFloor = pygame.image.load('images/Top_edge_floor.jpg').convert()
-
-cFloor = pygame.image.load('images/Left_corner_floor.jpg').convert()
-
+img = pygame.image.load('level/bot_wall.jpg').convert()
+wallDown = pygame.image.load('level/Side_Walls_05.jpg').convert()
+sFloor = pygame.image.load('level/Left_edge_floor.jpg').convert()
+floor = pygame.image.load('level/floor.jpg').convert()
+tFloor = pygame.image.load('level/Top_edge_floor.jpg').convert()
+cFloor = pygame.image.load('level/Left_corner_floor.jpg').convert()
 
 
 #----------Put character in starting position------------
 player.rect.x = int(width/2)
 player.rect.y = int(height-player.rect.height-20)
 
+
 #---------------------Game Loop--------------------------
+tabCount = 0
+chosenElement = element[0]      #defualt to fire
 while 1:
+
+    pygame.event.pump()
+
     xCounter = yCounter = 0
-    for event in pygame.event.get():    #event handler, just used to quit for now
+    for event in pygame.event.get():    #event handler, checks for key presses (not holds)
         if event.type == pygame.QUIT:
             exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_TAB:
+                tabCount+=1
+                if tabCount >= len(element):
+                    tabCount = 0
+                chosenElement = element[tabCount]
+                print(tabCount)
+            elif event.key == pygame.K_SPACE:
+                magic.add(projectiles.projectiles(player, chosenElement, direction, enemies))
+                
+                
 
     screen.fill((0,0,0))                #reset screen, for clean animation
 
     levels.makeLevel(screen, img, wallDown, sFloor, floor, tFloor, cFloor)
-    screen.blit(move(pygame.key.get_pressed(), player, uAnim, dAnim, lAnim, rAnim,
-                     animations = (upAnim, downAnim, leftAnim, rightAnim)), player.rect)
+    direction = move(pygame.key.get_pressed(), player, uAnim, dAnim, lAnim, rAnim,
+                     animations = (upAnim, downAnim, leftAnim, rightAnim))
 
-    pygame.display.flip()
-    clock.tick(60)
+    magic.update()          #projectiles group
+    enemyGroup.draw(screen)     #same as blit, but draws the entire group
+    magic.draw(screen)          #draws all projectiles to screen
+    screen.blit(player.image, player.rect)      #will put the 'hero' in its own group soon
+    pygame.display.flip()   #ACTUALLY display all the images
+    clock.tick(60)      #all animation and timing is based on this 60fps counter
